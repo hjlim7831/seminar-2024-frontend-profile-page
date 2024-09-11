b8e66809127a9cc19637ea851f831164930e4342
임혜진 <hjlim7831@gmail.com>
2024-09-11 22:32:29 +0900

    docs: prettier style 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,57 @@
 import './App.css';
 
-import { Avatar, Box, Card, createTheme, Stack, ThemeProvider, Typography } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Card,
+  createTheme,
+  Stack,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
 
 import AchievementList from './components/AchievementList';
 
 const theme = createTheme({
   typography: {
-    fontFamily: "Pretendard",
-  }
-})
+    fontFamily: 'Pretendard',
+  },
+});
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <Box display="flex" justifyContent="center" marginTop="20px">
         <Stack direction="row" spacing={2}>
-          <Box sx={{paddingTop: "10px"}}>
-            <Avatar src="./carrot_anonymous.jpg" sx={{width: "200px", height: "200px"}}/>
-            <Stack direction="column" spacing={"1px"}>
-              <Typography variant="h5" fontWeight={500}>임혜진</Typography>
+          <Box sx={{ paddingTop: '10px' }}>
+            <Avatar
+              src="./carrot_anonymous.jpg"
+              sx={{ width: '200px', height: '200px' }}
+            />
+            <Stack direction="column" spacing={'1px'}>
+              <Typography variant="h5" fontWeight={500}>
+                임혜진
+              </Typography>
               <Typography>#지구환경과학부</Typography>
               <Typography>#IN?F?P?</Typography>
               <Typography>#대학원_탈주</Typography>
               <Typography></Typography>
             </Stack>
           </Box>
-          <Card sx={{ width: 400}}>
-            <Typography sx={{ padding: "10px"}}  variant="h5" component="div" fontWeight={900}>인생 업적</Typography>
-            <AchievementList/>
+          <Card sx={{ width: 400 }}>
+            <Typography
+              sx={{ padding: '10px' }}
+              variant="h5"
+              component="div"
+              fontWeight={900}
+            >
+              인생 업적
+            </Typography>
+            <AchievementList />
           </Card>
         </Stack>
       </Box>
     </ThemeProvider>
-    
   );
 }
 
--- src/components/AchievementItem.tsx
+++ src/components/AchievementItem.tsx
@@ -1,28 +1,32 @@
-import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 export interface AchievementItemProps {
-    title: string;
-    description: string;
-    color?: string | number;
+  title: string;
+  description: string;
+  color?: string | number;
 }
 
-const AchievementItem = ({ title, description, color }: AchievementItemProps) => {
-    const avatarSx = {
-        bgcolor: color !== undefined ? color: grey[400]
-    }
+const AchievementItem = ({
+  title,
+  description,
+  color,
+}: AchievementItemProps) => {
+  const avatarSx = {
+    bgcolor: color !== undefined ? color : grey[400],
+  };
 
-    return (
-        <ListItem>
-            <ListItemAvatar>
-                <Avatar sx={avatarSx}>
-                    <EmojiEventsIcon />
-                </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={title} secondary={description}/>
-        </ListItem>
-    )
-}
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar sx={avatarSx}>
+          <EmojiEventsIcon />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={title} secondary={description} />
+    </ListItem>
+  );
+};
 
 export default AchievementItem;
